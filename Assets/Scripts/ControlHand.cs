b005a75c7fd8f88c3ebff5aef92a6afe881f4c81
Sham Shaar <130391798+shaarsham@users.noreply.github.com>
2024-12-23 21:52:10 +0100

    latest update

--- Assets/Scripts/ControlHand.cs
+++ Assets/Scripts/ControlHand.cs
@@ -1,14 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class ControlHand : MonoBehaviour
 {
-    public Transform hand;  
-    public float moveSpeed = 2f;  
+    public Transform hand; 
+    public Camera playerCamera;  
+    public Vector3 handOffset = new Vector3(0f, 120f, -10f);  
+    public float mouseSensitivity = 100f;  
+    private float xRotation = 0f; 
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
     void Update()
     {
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-        hand.localPosition += move * moveSpeed * Time.deltaTime;
+       
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        playerCamera.transform.Rotate(Vector3.up * mouseX);
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); 
+        playerCamera.transform.localRotation = Quaternion.Euler(xRotation, playerCamera.transform.localRotation.eulerAngles.y, 0f);
+
+        hand.position = handOffset + playerCamera.transform.position;
+
+        hand.rotation = playerCamera.transform.rotation * Quaternion.Euler(0f, 180f, 0f);  
     }
 }
